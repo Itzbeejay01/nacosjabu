e6eb1b92a90088f194a9d4896ae5cb1db092e131
Omolayoayokanmi <97984877+Itzbeejay01@users.noreply.github.com>
2023-12-15 22:18:05 +0100

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,15 @@
+
+//MENU SHOW
+function menuShow (){
+  const myButton = document.getElementById('navmenu')
+  const myNavbar = document.getElementById('navbar')
+
+  function togglemenu () {
+      myNavbar.classList.toggle('hidden')
+      myNavbar.classList.toggle('flex')
+  }
+  myButton.addEventListener('click', togglemenu)
+  myNavbar.addEventListener('click', togglemenu)
+} 
+document.addEventListener('DOMContentLoaded', menuShow)
+
